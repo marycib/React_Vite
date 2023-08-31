5393e9982a282f93fe81500310111a49ba6fcd1e
marycib <mary_qui@outlook.es>
2023-08-30 20:11:35 -0500

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div className="bg-indigo-500">
+        Hola Mundo
+        <button class="bg-sky-500/100 ..."></button>
+      </div>
+
+    
+  )
+}
+
+export default App
